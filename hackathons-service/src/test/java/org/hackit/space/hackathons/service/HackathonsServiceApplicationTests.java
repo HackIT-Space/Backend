bcbe1f46a9b3d6e148a9952ef0818484868353be
Vasily Nikitin <vasyan.coder@gmail.com>
2025-04-14 18:14:01 +0300

    feat: init hackathons-service

---
+++ hackathons-service/src/test/java/org/hackit/space/hackathons/service/HackathonsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackit.space.hackathons.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
